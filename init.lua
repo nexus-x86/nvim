d815a1c9d31c5b4cf0fee87891e526692aaa4af4
nexus-x86 <60083031+nexus-x86@users.noreply.github.com>
2026-03-18 00:02:59 -0400

    config changes

--- init.lua
+++ init.lua
@@ -1,2 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.colorcolumn = "80"
+
+vim.o.number = true
+vim.o.undofile = true
+vim.o.confirm = true
